f5ebf44066a9ff90db74908b200b155bb9cfcdef
Nels Schimek <nlsschim@uw.edu>
2023-04-14 14:19:55 -0700

    read the docs sphinx

--- diff_viz/testing_module.py
+++ diff_viz/testing_module.py
@@ -1,5 +1,11 @@
 import numpy as np
 
-def test_func():
+def test_func(x, y):
+
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     array = np.array([1,2,3,4])
     return array
