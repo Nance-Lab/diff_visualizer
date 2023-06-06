9fee176345457539aa56e93de98c8ef668093ce5
Nels Schimek <nlsschim@uw.edu>
2023-06-05 17:13:20 -0700

    bug fix

--- pages/02_msd_plotting.py
+++ pages/02_msd_plotting.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.session_state.update(st.session_state)
 st.header('MSD Plotting')
 
-if 'DATA_LOADED' is True:
+if 'DATA_LOADED' == True:
     st.write('Sorry, this feature is not yet available! - Brendan and Nels')
 else:
     st.error('Please upload a valid CSV file using the File Uploader')
