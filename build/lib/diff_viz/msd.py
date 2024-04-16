ecc3d9be18ec0ac71e97bd231a7f28e9ae1c8314
Nels Schimek <nlsschim@uw.edu>
2024-04-16 12:06:03 -0700

    l

---
+++ build/lib/diff_viz/msd.py
@@ -0,0 +1,189 @@
+"""
+Visualize mean-squared displacement (MSD) data from diffusivity experiments.
+ """
+import os
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import numpy.ma as ma
+import matplotlib.pyplot as plt
+from diff_viz import diff_utils
+
+from os import listdir, getcwd, chdir
+from os.path import isfile, join
+
+#from diff_utils import get_experiment, get_path, get_csvs, get_geo_dict, get_geo_df, get_df_dose_list, calc_error
+
+def plot_individual_msds(df, x_range=10, y_range=100, umppx=0.16, fps=100.02, alpha=0.1,
+                          figsize=(10, 10), subset=False, size=1000,
+                         dpi=300, title=None):
+    """
+    Plot MSDs of trajectories and the geometric average.
+
+    Parameters
+    ----------
+    prefix: string
+        Prefix of file name to be plotted e.g. features_P1.csv prefix is P1.
+    x_range: float64 or int
+        Desire x range of graph.
+    y_range: float64 or int
+        Desire y range of graph.
+    fps: float64
+        Frames per second of video.
+    umppx: float64
+        Resolution of video in microns per pixel.
+    alpha: float64
+        Transparency factor.  Between 0 and 1.
+    upload: boolean
+        True if you want to upload to s3.
+
+    Returns
+    -------
+    geo_mean: numpy array
+        Geometric mean of trajectory MSDs at all time points.
+    geo_SEM: numpy array
+        Geometric standard errot of trajectory MSDs at all time points.
+
+    """
+
+    merged = df
+
+    fig = plt.figure(figsize=figsize)
+    particles = int(max(merged['Track_ID']))
+
+    if particles < size:
+        size = particles - 1
+    else:
+        pass
+
+    frames = int(max(merged['Frame']))
+
+    y = merged['Y']#.values.reshape((particles+1, frames+1))*umppx*umppx
+    x = merged['X']#.values.reshape((particles+1, frames+1))/fps
+#     for i in range(0, particles+1):
+#         y[i, :] = merged.loc[merged.Track_ID == i, 'MSDs']*umppx*umppx
+#         x = merged.loc[merged.Track_ID == i, 'Frame']/fps
+
+    particles = np.linspace(0, particles, particles-1).astype(int)
+    if subset:
+        particles = np.random.choice(particles, size=size, replace=False)
+
+    y = np.zeros((particles.shape[0], frames+1))
+    for idx, val in enumerate(particles):
+        y[idx, :] = merged.loc[merged.Track_ID == val, 'MSDs']*umppx*umppx
+        x = merged.loc[merged.Track_ID == val, 'Frame']/fps
+        plt.plot(x, y[idx, :], 'k', alpha=alpha)
+
+    geo_mean = np.nanmean(ma.log(y), axis=0)
+    geo_SEM = stats.sem(ma.log(y), axis=0, nan_policy='omit')
+    plt.plot(x, np.exp(geo_mean), 'k', linewidth=4)
+    plt.plot(x, np.exp(geo_mean-geo_SEM), 'k--', linewidth=2)
+    plt.plot(x, np.exp(geo_mean+geo_SEM), 'k--', linewidth=2)
+    plt.xlim(0, x_range)
+    plt.ylim(0, y_range)
+    plt.xlabel('Tau (s)', fontsize=25)
+    plt.ylabel(r'Mean Squared Displacement ($\mu$m$^2$)', fontsize=25)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.tight_layout()
+    plt.title(title, fontsize=25)
+    plt.show()
+    return fig
+
+def msd_viz(doses,geomean_df,geosem_df,fps):
+    """
+    Visualize MSD data from diffusivity experiments.
+    
+    Parameters
+    ----------
+        doses (list):
+            List of doses used in the experiment.
+        geomean_df (pandas.DataFrame):
+            DataFrame containing the mean-squared displacement data.
+        geosem_df (pandas.DataFrame):
+            DataFrame containing the standard error of the mean-squared displacement data.
+        fps (int):
+            Frames per second of the experiment.
+
+    Returns
+    -------
+        fig (matplotlib.figure.Figure):
+            Figure containing the MSD data.
+
+    """
+    count=0
+    msd_dose_list=diff_utils.get_df_dose_list(doses,geomean_df)
+    sem_dose_list=diff_utils.get_df_dose_list(doses,geosem_df)
+    
+    if len(doses)==1:
+        tau=geomean_df.index.values/651
+        fig,ax=plt.subplots(figsize=(6,6))
+        plt.rcParams.update({'font.family':'helvetica'})
+        handles=list(geomean_df.columns)
+    
+        for handle in handles:    
+            ax.loglog(tau,geomean_df[handle])
+            ax.set_xlabel('Lag time (s)',fontsize=16)
+            ax.set_ylabel('Mean-squared displacement (μ$m^2$)',fontsize=16)
+            ax.fill_between(tau,geomean_df[handle]+geosem_df[handle]/2,geomean_df[handle]-geosem_df[handle]/2,cmap='ocean',alpha=0.2)
+            ax.legend(handles,loc='best')
+            ax.set_xlim([0.008, 0.2])
+            ax.set_ylim([0.008,1])
+            #ax.set_title(f'\n{doses[c]}\n', fontsize=16)
+         
+    else:                
+        tau=geomean_df.index.values/651
+        fig,axes=plt.subplots(nrows=1,ncols=len(doses),figsize=(len(doses)*6,6))
+        plt.rcParams.update({'font.family':'helvetica'})
+        count=0
+        
+        for c in np.arange(0,len(doses)):
+            while count<=c:
+                sem_handles = list(sem_dose_list[c].columns)
+                msd_handles = list(msd_dose_list[c].columns)
+                axes[c].loglog(tau,msd_dose_list[count])
+                axes[c].fill_between(tau,msd_dose_list[count]+sem_dose_list[count]/2,msd_dose_list[count]-sem_dose_list[count]/2,cmap='ocean',alpha=0.2)
+                axes[c].set_xlabel('\nLag time (s)',fontsize=16)
+                #axes[c].legend(handles,loc='best')
+                axes[c].set_xlim([0.008, 1])
+                axes[c].set_ylim([0.008,1])
+                axes[c].set_title(f'\n{doses[c]}\n', fontsize=16)
+    
+                if c == 0:
+                    axes[c].set_ylabel('Mean-squared displacement (μ$m^2$)',fontsize=16)
+    
+                count+=1
+    return fig
+
+
+def get_geo_data(merged, umppx=0.16, fps=100.02, size=1000, frames=651):
+
+    "takes in a df and returns the geo mean and     geo SEM"
+
+    particles = int(max(merged['Track_ID']))
+
+    if particles < size:
+        size = particles - 1
+    else:
+        pass
+
+    #frames = int(max(merged['Frame']))
+
+    #y = merged['Y'].values.reshape((particles+1, frames+1))*umppx*umppx
+    #x = merged['X'].values.reshape((particles+1, frames+1))/fps
+#     for i in range(0, particles+1):
+#         y[i, :] = merged.loc[merged.Track_ID == i, 'MSDs']*umppx*umppx
+#         x = merged.loc[merged.Track_ID == i, 'Frame']/fps
+
+    particles = np.linspace(0, particles, particles-1).astype(int)
+
+    y = np.full(shape=(particles.shape[0], frames), fill_value=np.nan)
+    for idx, val in enumerate(particles):
+        y_data = merged.loc[merged.Track_ID == val, 'MSDs']*umppx*umppx
+        y[idx, 0:len(y_data)] = y_data#merged.loc[merged.Track_ID == val, 'MSDs']*umppx*umppx
+        x = merged.loc[merged.Track_ID == val, 'Frame']/fps
+
+    geo_mean = np.nanmean(ma.log(y), axis=0)
+    geo_SEM = stats.sem(ma.log(y), axis=0, nan_policy='omit')
+
+    return geo_mean, geo_SEM
---
+++ build/lib/diff_viz/pca_plots.py
@@ -0,0 +1,133 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import scale, StandardScaler
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+from sklearn.decomposition import PCA
+
+
+def pca_plot(df, n_components=2, labels=None, scale=True, plot=True, save=False, save_path=None):
+    """
+    Function to perform PCA on a dataframe and plot the results. 
+
+    Parameters
+    ----------
+        df (pd.DataFrame): Dataframe containing the data to plot
+        n_components (int): Number of principal components to plot
+        labels (String): column name of the labels in the dataframe. If None, no labels will be plotted
+        scale (bool): Whether to scale the data before performing PCA
+        plot (bool): Whether to plot the results
+        save (bool): Whether to save the plot
+        save_path (String): Path to save the plot
+
+    Returns
+    -------
+        fig: matplotlib figure of the PCA plot
+    """
+    if labels:
+        label_col = df[labels]
+        df = df.drop(labels, axis=1)
+    # Scale the data 
+    if scale:
+        df = StandardScaler().fit_transform(df)
+    # Perform PCA 
+    pca = PCA(n_components=n_components)
+    pca.fit(df)
+    # Get the principal components 
+    principal_components = pca.transform(df)
+    # Create a dataframe of the principal components 
+    principal_df = pd.DataFrame(principal_components[:, :2], columns=['Component 1', 'Component 2'])
+    principal_df[labels] = label_col
+    # Get the explained variance 
+    explained_variance = pca.explained_variance_ratio_
+    # Plot the results 
+    if plot:
+        plt.figure(figsize=(10, 10))
+        plt.xlabel('PC1')
+        plt.ylabel('PC2')
+        plt.title('PCA Plot')
+        for label in np.unique(label_col):
+            print(label)
+            label_df = principal_df[principal_df[labels]==label]
+            plt.scatter(label_df['Component 1'], label_df['Component 2'], label=label, alpha=0.5, s=4)
+        plt.legend(loc='lower left')
+        plt.xlim([-13,13])
+        plt.ylim([-13,13])
+        #plt.show()
+    # Save the plot 
+    if save:
+        plt.savefig(save_path)
+    # Return the explained variance 
+    return plt.gcf(), explained_variance
+
+def plot_pca_bi_plot(df, 
+                     n_components=2, 
+                     features=None, 
+                     target_col=None, 
+                     num_points='all', 
+                     title='PCA Biplot of First and Second Principal Components'):
+    """
+    Function to plot a biplot of the PCA results.
+
+    Parameters
+    ----------
+        score (np.array): Array of the PCA scores
+        coeff (np.array): Array of the PCA coefficients
+        labels (list): List of the feature names
+        target_col (string): Name of the column containing the labels
+        num_points (int): Number of points to plot. If 'all', all points will be plotted
+
+    Returns
+    -------
+        fig: matplotlib figure of the biplot
+
+    """
+    fig = plt.figure(figsize=(12,8))
+
+    if target_col:
+        labels = np.array(df[target_col])
+        label_col = df[target_col]
+        df = df.drop(target_col, axis=1)
+    
+    # Scale the data 
+    df = StandardScaler().fit_transform(df)
+    # Perform PCA 
+    pca = PCA(n_components=n_components).fit(df)
+    # Get the principal components 
+    principal_components = pca.transform(df)
+    # Create a dataframe of the principal components 
+    principal_df = pd.DataFrame(principal_components[:, :2], columns=['Component 1', 'Component 2'])
+    principal_df[target_col] = label_col
+    # Get the explained variance
+    score = principal_components[:, 0:2]
+    coeff = np.transpose(pca.components_[0:2, :])
+
+    xs = score[:,0]
+    ys = score[:,1]
+    n = coeff.shape[0]
+    scalex = 1.0/(xs.max() - xs.min())
+    scaley = 1.0/(ys.max() - ys.min())
+    for uclass in np.unique(labels):
+        x = (xs[labels==uclass])*scalex
+        y = (ys[labels==uclass])*scaley
+        if num_points == 'all':
+            plt.scatter(x, y, alpha=0.5, s=1, label=uclass)
+        else:
+            inds = np.random.randint(0, len(x), num_points)
+            plt.scatter(x[inds], y[inds], alpha=0.5, s=1, label=uclass)
+    #plt.scatter(xs * scalex,ys * scaley)#, c = y)
+    for i in range((n//2), n):
+        plt.arrow(0, 0, coeff[i,0], coeff[i,1],color = 'r',alpha = 0.5)
+        if features is None:
+            plt.text(coeff[i,0]* 1.15, coeff[i,1] * 1.15, "Var"+str(i+1), color = 'k', ha = 'center', va = 'center')
+        else:
+            plt.text(coeff[i,0], coeff[i,1], features[i], color = 'k', ha = 'center', va = 'center')
+    plt.xlim(-0.6,.6)
+    plt.ylim(-.6,.6)
+    plt.xlabel(f"PC{1}")
+    plt.ylabel(f"PC{2}")
+    plt.grid()
+    plt.legend(loc='lower left')
+    plt.title(title)
+    #fig = plt.gcf()
+    return fig
